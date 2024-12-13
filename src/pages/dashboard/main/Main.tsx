fc3319583a6b2ca8437bfd5ce035aafead6b99bb
mi <musailliasu7@gmail.com>
2024-12-13 23:23:24 +0100

    main card layout

--- src/pages/dashboard/main/Main.tsx
+++ src/pages/dashboard/main/Main.tsx
@@ -2,7 +2,17 @@
 const Main = () => {
 
   return (
-    <div>Main</div>
+    <>
+      <h2 className="font-bold md:text-lg mb-4">Dashboard</h2>
+      <p className="text-[#2a2f5b] mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+        <div className="min-h-[150px] bg-white rounded-lg shadow"></div>
+        <div className="min-h-[150px] bg-white rounded-lg shadow"></div>
+        <div className="min-h-[150px] bg-white rounded-lg shadow"></div>
+        <div className="min-h-[150px] bg-white rounded-lg shadow"></div>
+      </div>
+    </>
   )
 }
 
